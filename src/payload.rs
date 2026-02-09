956a07141dca59c0472380cba6256c1cd80cc139
Sean Outram <me@sean.cyou>
2026-02-09 11:47:10 +0000

    Finish parsing xml messages

--- src/payload.rs
+++ src/payload.rs
@@ -4,7 +4,7 @@ pub fn handle_payload(payload: &[u8]) -> anyhow::Result<()> {
     let payload_str = std::str::from_utf8(payload)?;
     // Parse the XML
     let xml = quick_xml::de::from_str::<PassengerTrainConsistMessage>(payload_str)?;
-    println!("XML: {:?}", xml);
+    println!("XML: {:#?}", xml);
     Ok(())
 }
 
@@ -13,9 +13,15 @@ struct PassengerTrainConsistMessage {
     #[serde(rename = "MessageHeader")]
     _message_header: MessageHeader,
     #[serde(rename = "MessageStatus")]
-    message_status: i32,
+    _message_status: i32,
     #[serde(rename = "TrainOperationalIdentification")]
-    train_operational_identification: TrainOperationalIdentification,
+    _train_operational_identification: TrainOperationalIdentification,
+    #[serde(rename = "OperationalTrainNumberIdentifier")]
+    _operational_train_number_identifier: OperationalTrainNumberIdentifier,
+    #[serde(rename = "ResponsibleRU")]
+    _responsible_ru: String,
+    #[serde(rename = "Allocation")]
+    _allocation: Option<Vec<Allocation>>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -39,24 +45,149 @@ struct MessageReference {
 #[derive(Deserialize, Debug)]
 struct TrainOperationalIdentification {
     #[serde(rename = "TransportOperationalIdentifiers")]
-    train_operational_identifiers: Vec<TrainOperationalIdentifiers>,
+    _train_operational_identifiers: Vec<TrainOperationalIdentifiers>,
 }
 
 #[derive(Deserialize, Debug)]
 struct TrainOperationalIdentifiers {
     #[serde(rename = "ObjectType")]
-    object_type: String,
+    _object_type: String,
     #[serde(rename = "Company")]
-    company: String,
+    _company: String,
     #[serde(rename = "Core")]
-    core: String,
+    _core: String,
     #[serde(rename = "Variant")]
-    variant: String,
+    _variant: String,
     #[serde(rename = "TimetableYear")]
-    timetable_year: String,
+    _timetable_year: String,
     #[serde(rename = "StartDate")]
-    start_date: String,
+    _start_date: String,
 }
 
 #[derive(Deserialize, Debug)]
-struct OperationalTrainNumberIdentifier {}
+struct OperationalTrainNumberIdentifier {
+    #[serde(rename = "OperationalTrainNumber")]
+    _operational_train_number: String,
+    #[serde(rename = "ScheduledTimeAtHandover")]
+    _scheduled_time_at_handover: String,
+    #[serde(rename = "ScheduledDateTimeAtTransfer")]
+    _scheduled_date_time_at_transfer: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Allocation {
+    #[serde(rename = "AllocationSequenceNumber")]
+    _allocation_sequence_number: i64,
+    #[serde(rename = "TrainOriginDateTime")]
+    _train_origin_date_time: String,
+    #[serde(rename = "TrainOriginLocation")]
+    _train_origin_location: TrainLocation,
+    #[serde(rename = "ResourceGroupPosition")]
+    _resource_group_position: i64,
+    #[serde(rename = "DiagramDate")]
+    _diagram_date: String,
+    #[serde(rename = "DiagramNo")]
+    _diagram_no: Option<String>,
+    #[serde(rename = "TrainDestLocation")]
+    _train_dest_location: TrainLocation,
+    #[serde(rename = "TrainDestDateTime")]
+    _train_dest_date_time: String,
+    #[serde(rename = "AllocationOriginLocation")]
+    _allocation_origin_location: TrainLocation,
+    #[serde(rename = "AllocationOriginDateTime")]
+    _allocation_origin_date_time: String,
+    #[serde(rename = "AllocationOriginMiles")]
+    _allocation_origin_miles: i64,
+    #[serde(rename = "AllocationDestinationLocation")]
+    _allocation_destination_location: TrainLocation,
+    #[serde(rename = "AllocationDestinationDateTime")]
+    _allocation_destination_date_time: String,
+    #[serde(rename = "AllocationDestinationMiles")]
+    _allocation_destination_miles: i64,
+    #[serde(rename = "Reversed")]
+    _reversed: String,
+    #[serde(rename = "ResourceGroup")]
+    _resource_group: ResourceGroup,
+}
+
+#[derive(Deserialize, Debug)]
+struct TrainLocation {
+    #[serde(rename = "CountryCodeISO")]
+    _country_code_iso: String,
+    #[serde(rename = "LocationPrimaryCode")]
+    _location_primary_code: String,
+    #[serde(rename = "LocationSubsidiaryIdentification")]
+    _location_subsidiary_identification: LocationSubsidiaryIdentification,
+}
+
+#[derive(Deserialize, Debug)]
+struct LocationSubsidiaryIdentification {
+    #[serde(rename = "LocationSubsidiaryCode")]
+    _location_sibsidiary_code: String,
+    #[serde(rename = "AllocationCompany")]
+    _allocation_company: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct ResourceGroup {
+    #[serde(rename = "ResourceGroupId")]
+    _resource_group_id: String,
+    #[serde(rename = "TypeOfResource")]
+    _type_of_resource: String,
+    #[serde(rename = "FleetId")]
+    _fleet_id: String,
+    #[serde(rename = "ResourceGroupStatus")]
+    _resource_group_status: String,
+    #[serde(rename = "EndOfDayMiles")]
+    _end_of_day_miles: String,
+    #[serde(rename = "Vehicle")]
+    _vehicle: Vec<Vehicle>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Vehicle {
+    #[serde(rename = "VehicleId")]
+    _vehicle_id: String,
+    #[serde(rename = "TypeOfVehicle")]
+    _type_of_vehicle: String,
+    #[serde(rename = "ResourcePosition")]
+    _resource_position: i64,
+    #[serde(rename = "PlannedResourceGroup")]
+    _planned_resource_group: String,
+    #[serde(rename = "SpecificType")]
+    _specific_type: String,
+    #[serde(rename = "Length")]
+    _length: Length,
+    #[serde(rename = "Weight")]
+    _weight: i32,
+    #[serde(rename = "Livery")]
+    _livery: String,
+    #[serde(rename = "Decor")]
+    _decor: String,
+    #[serde(rename = "SpecialCharacteristics")]
+    _special_characteristics: Option<String>,
+    #[serde(rename = "NumberOfSeats")]
+    _number_of_seats: i32,
+    #[serde(rename = "RegisteredStatus")]
+    _registered_status: String,
+    #[serde(rename = "Cabs")]
+    _cabs: Option<i32>,
+    #[serde(rename = "DateEnteredService")]
+    _date_entered_service: String,
+    #[serde(rename = "DateRegistered")]
+    _date_registered: String,
+    #[serde(rename = "RegisteredCategory")]
+    _registered_category: String,
+    #[serde(rename = "TrainBrakeType")]
+    _train_brake_type: String,
+    #[serde(rename = "MaximumSpeed")]
+    _maximum_speed: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Length {
+    #[serde(rename = "Value")]
+    _value: String,
+    #[serde(rename = "Measure")]
+    _measure: String,
+}
